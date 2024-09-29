325da4b53e94999fcf0dcab5f69d818c575d2e94
Israel Reyes <42876025+Israel-r@users.noreply.github.com>
2024-09-29 12:11:05 -0600

    Create lineups.js

---
+++ server/routes/lineups.js
@@ -0,0 +1,8 @@
+// server/routes/lineups.js
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+// ... similar routes for defensive lineups
+
+module.exports = router;
